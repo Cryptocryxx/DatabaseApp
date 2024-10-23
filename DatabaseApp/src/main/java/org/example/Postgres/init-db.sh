1821bec591fefa7fbd663537c69b0058d3d02d1f
Cryptocryxx <143177997+Cryptocryxx@users.noreply.github.com>
2024-10-23 12:30:57 +0200

    fixed init-db.sh problems

--- DatabaseApp/src/main/java/org/example/Postgres/init-db.sh
+++ DatabaseApp/src/main/java/org/example/Postgres/init-db.sh
@@ -4,19 +4,13 @@ set -e
 # Warten, bis die PostgreSQL-Datenbank verfügbar ist
 echo "Warte auf PostgreSQL-Datenbank..." 
 
-psql -v ON_ERROR_STOP=1 --username user << EOSQL
-
-        CREATE USER user WITH PASSWORD 'password';
-        CREATE DATABASE my_database;
-        GRANT ALL PRIVILEGES ON DATABASE my_database TO user;
+psql -v ON_ERROR_STOP=1 --username "user" --dbname="my_database" << EOSQL
 
 EOSQL
 
-psql -v ON_ERROR_STOP=1 --username user \
-  --dbname=my_database --command "GRANT ALL ON SCHEMA public TO user"
 
 psql -v ON_ERROR_STOP=1 --username user --dbname=my_database \
   --file /home/SQLData/tableSchema.sql
 
-psql -v ON_ERROR_STOP=1 --username user --dbname=my_database \
-  --file /home/SQLData/insertData.sql
+  psql -v ON_ERROR_STOP=1 --username user --dbname=my_database \
+    --file /home/SQLData/insertData.sql
